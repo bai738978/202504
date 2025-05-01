aa3cfcf4ee23627d1c79dae1fc87e127a7bbea94
bai738978 <a0965738978@gmail.com>
2025-05-01 13:09:00 +0800

    prefab

--- Assets/sciprts/CharacterContoller.cs
+++ Assets/sciprts/CharacterContoller.cs
@@ -2,27 +2,30 @@ using Codice.Client.BaseCommands;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class charatercontrler : MonoBehaviour
+public class CharacterContoller : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     [SerializeField]
     private PlayerInput playerInput;
-    
+
+    [SerializeField]
+    private float moveSpeed = 3f;
+
     private InputAction moveAction;
 
     private void Start()
     {
-        moveAction = playerInput.actions.FindAction["Move"];
+        moveAction = playerInput.actions.FindAction("Move");
     }
 
     // Update is called once per frame
     void Update()
     {
-           
-        var moveVector2 = moveAction.ReadValue<Vector2>() 
-        
+        var moveVector2 = moveAction.ReadValue<Vector2>();
+
         // 修正斜著走比較快的問題
-        var direction = new Vector3(moveVector2.x,  moveVector2.y , 0);
+        var direction = new Vector3(moveVector2.x , moveVector2.y , 0);
+        var movement  = direction * moveSpeed * Time.deltaTime;
         transform.position += movement;
     }
 }
